556e269a7bd899aa1d1dc7b000e997d0c959c0fc
ZetaPitu <35515984+ZetaPitu@users.noreply.github.com>
2018-04-16 04:41:43 +0200

    Aplicaciiones para movil de Sirvertalent

---
+++ Movil/www/Admin.php
@@ -0,0 +1,98 @@
+<?php
+
+?><!DOCTYPE html>
+<html >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.7.1, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.7.1, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logoletrasplata-109x82.png" type="image/x-icon">
+  <meta name="description" content="Pagina de solo acceso a lo administradores y que controla todo">
+  <title>Administrador</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700&subset=latin,cyrillic">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alegreya+Sans:400,700&subset=latin,vietnamese,latin-ext">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/soundcloud-plugin/style.css">
+  <link rel="stylesheet" href="assets/puritym/css/style.css">
+  <link rel="stylesheet" href="assets/dropdown-menu/style.light.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+   <link rel="stylesheet" href="assets/mobirise/css/mbr-mios.css" type="text/css">
+<script src="assets/dropdown-menu/mios.js"></script>
+</head>
+<body>
+  <section id="dropdown-menu-1y" data-rv-view="186">
+
+    <nav class="navbar navbar-dropdown navbar-fixed-top">
+
+        <div class="container">
+
+            <div class="navbar-brand">
+                <a href="index.html" class="navbar-logo"><img src="assets/images/logoletrasplata-109x82.png" alt="Mobirise"></a>
+                <a class="text-primary" href="index.html">Inicio</a>
+            </div>
+
+            <button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                <div class="hamburger-icon"></div>
+            </button>
+
+            <ul class="nav-dropdown collapse pull-xs-right navbar-toggleable-sm nav navbar-nav" id="exCollapsingNavbar"><li class="nav-item dropdown"><a class="nav-link link dropdown-toggle" href="https://mobirise.com/" aria-expanded="false" data-toggle="dropdown-submenu">🎎Área de candidatos</a><div class="dropdown-menu"><a class="dropdown-item" href="Enviar_CV.html">📄Enviar CV</a><a class="dropdown-item" href="Registro_candidatos.html">📟Registro</a></div></li><li class="nav-item dropdown open"><a class="nav-link link dropdown-toggle" href="https://mobirise.com/" aria-expanded="true" data-toggle="dropdown-submenu">🏭Área de empresas</a><div class="dropdown-menu"><a class="dropdown-item" href="BolsadeTrabajo.html">📐Bolsa de Trabajo</a><a class="dropdown-item" href="Publicar_Oferta.html">📋Publicar oferta laboral</a><a class="dropdown-item" href="Registro_Empresas.html">📓Registro empresas</a></div></li><li class="nav-item"><a class="nav-link link" href="Publicidad.html" aria-expanded="false">📊Publicidad</a></li><li class="nav-item"><a class="nav-link link" href="Contacto.html" aria-expanded="false">📧Contacto</a></li><li class="nav-item"><a class="nav-link link" href="Blog.html" aria-expanded="false">📱Blog</a></li><li class="nav-item nav-btn"><a class="nav-link btn btn-danger-outline btn-danger" href="Inicio_Sesion.html">Inicio sesión</a></li></ul>
+
+        </div>
+
+    </nav>
+
+</section>
+
+<section class="mbr-section mbr-section-full mbr-after-navbar" id="header2-2e" data-rv-view="188" style="background-color: rgb(255, 255, 255);">
+    <div class="mbr-table-cell">
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 text-xs-center">
+
+                    <h1 class="mbr-section-title display-1">Esta pagina sera &nbsp;de los administradores<br>Esta pagina se Diseña en PHP y tendra acceso a todo&nbsp;</h1>
+                    <p class="lead"><br>El diseño en php es para poner controles de seguridad en el acceso a datos<br>cuentas bancarias ,etc....... Se restringuira el acceso a cual ordenador,movil o tablet que no este<br>verificado por el administrador. Esta pagina que sea mas atractactiva o menos es igual El trabajo se centrara en la seguridad.<br>Sera entrara con contraseña y podra modificar :<br><strong>Habra tres tipos de administradores</strong><br>&nbsp;Primero que seras tu Total acceso a todo<br>&nbsp;Segundo que sere yo o otra persona que elijas Tendra acceso al diseño<br>&nbsp;Futuros empleados que tendran acceso a lo que el administrador le haya concedido</p>
+                    <div class="mbr-buttons--center"><a class="btn btn-lg btn-warning" href="index.html">Ver como usuario</a></div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</section>
+
+<section class="mbr-section mbr-section-small mbr-footer" id="contacts1-1z" data-rv-view="191" style="background-color: rgb(55, 56, 62); padding-top: 4.5rem; padding-bottom: 4.5rem;">
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-3">
+                <div><a href="#top"><img src="assets/images/logoletrasplata-109x82.png"></a></div>
+            </div>
+            <div class="col-xs-12 col-md-3">
+                <p>🏠<strong>Direccion</strong><br>Tu direccion<br>Ciudad</p>
+            </div>
+            <div class="col-xs-12 col-md-3">
+                <p><strong>Contacto</strong><br>📧 &nbsp; support@SirverTalent.es<br>
+☎ &nbsp; &nbsp;+1 (0) 000 0000 001<br>
+📠 +1 (0) 000 0000 002</p>
+            </div>
+            <div class="col-xs-12 col-md-3"><strong>Enlaces</strong><br><ul><li><a href="https://silvertalent.es/" class="text-primary" target="_blank">Sirvertalent</a></li><li><br></li><li><br></li></ul></div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smooth-scroll/SmoothScroll.js"></script>
+  <script src="assets/puritym/js/script.js"></script>
+  <script src="assets/dropdown-menu/script.js"></script>
+  
+  
+</body>
+</html>
